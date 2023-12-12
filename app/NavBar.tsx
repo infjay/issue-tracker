4c8d230b6c0d86a0ddab9301397bda15d4374cbc
infjay <41361157+infjay@users.noreply.github.com>
2023-12-12 21:11:45 +0100

    style the active link in then navBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,21 +1,38 @@
-import { link } from 'fs';
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 import { BsBug } from "react-icons/bs";
+import classnames from "classnames"
 const NavBar = () => {
-    const links =[
-        {label:"Dashboard",href:"/" },
-        {label:"Issues",href:"/issues" },
-    ]
-  return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/"><BsBug/></Link>
-        <ul className='flex space-x-6'>
-            {links.map(link => <Link key={link.href} className='text-zinc-500 hover:text-zinc-800 transiti' href={link.href}>{link.label}</Link>)}
-            
-        </ul>
-    </nav>
-  )
-}
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
 
-export default NavBar
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <BsBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classnames({
+                "text-zinc-900" : link.href === currentPath,
+                "text-zinc-500" : link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
